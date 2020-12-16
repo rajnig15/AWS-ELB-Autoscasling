12a79ec1942309a79697fe8e86785833efa63fd5
rajnig15 <48977973+rajnig15@users.noreply.github.com>
2020-12-16 11:22:21 +0530

    Update script1.sh
    Updated Script1

--- terraform/script1.sh
+++ terraform/script1.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+sudo su -
 script logfile1
 yum install httpd php php-mysql php-gd php-xml mariadb-server mariadb php-mbstring -y
-systemctl restart httpd.service ; systemctl enable httpd.service
-systemctl start mariadb ; systemctl enable mariadb
+systemctl restart httpd.service
+systemctl enable httpd.service
+systemctl start mariadb
+systemctl enable mariadb
 yum install -y amazon-linux-extras
 amazon-linux-extras enable php7.4
 yum clean metadata
@@ -22,16 +25,11 @@ CREATE USER 'wiki'@'localhost' IDENTIFIED BY 'THISpasswordSHOULDbeCHANGED' ;
 CREATE DATABASE wikidatabase ;
 GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost' ;
 FLUSH PRIVILEGES ;
-SHOW DATABASES ;
-SHOW GRANTS FOR 'wiki'@'localhost' ;
 EOF
-systemctl enable mariadb
-systemctl enable httpd
 wget https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1.35.0.tar.gz
 wget https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1.35.0.tar.gz.sig
 gpg --verify mediawiki-1.35.0.tar.gz.sig mediawiki-1.35.0.tar.gz
-tar -zxf mediawiki-1.35.0.tar.gz
-mv mediawiki-1.35.0.tar.gz /var/www/html/mediawiki
-tar -zxf /home/ec2-user/mediawiki-1.35.0.tar.gz
-chown -R apache:apache /var/www/mediawiki-1.35.0 
+tar -zxf /home/ec2-user/mediawiki-1.35.0.tar.gz -C /var/www/html/
+mv /var/www/html/mediawiki-1.35.0 /var/www/html/mediawiki
+chown -R apache:apache /var/www/html/mediawiki 
 service httpd restart
