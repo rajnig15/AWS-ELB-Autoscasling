5c54a1984c702d73e92a1bcd5ac9f205844f526e
Rajni Gupta <rajni.gupta.rajni@gmail.com>
2020-12-16 19:19:11 +0530

    added script1 file

---
+++ terraform/script1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+yum install httpd php php-mysql php-gd php-xml mariadb-server mariadb php-mbstring -y
+systemctl restart httpd.service
+systemctl enable httpd.service
+systemctl start mariadb
+systemctl enable mariadb
+yum install -y amazon-linux-extras
+amazon-linux-extras enable php7.4
+yum clean metadata
+yum install php -y
+mysql_secure_installation <<EOF
+
+y
+secret
+secret
+y
+y
+y
+y
+EOF
+mysql -u root -psecret << EOF > /tmp/sql123.log
+CREATE USER 'wiki'@'localhost' IDENTIFIED BY 'THISpasswordSHOULDbeCHANGED' ;
+CREATE DATABASE wikidatabase ;
+GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost' ;
+FLUSH PRIVILEGES ;
+EOF
+wget https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1.35.0.tar.gz
+wget https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1.35.0.tar.gz.sig
+gpg --verify mediawiki-1.35.0.tar.gz.sig mediawiki-1.35.0.tar.gz
+tar -zxf mediawiki-1.35.0.tar.gz -C /var/www/html/
+mv /var/www/html/mediawiki-1.35.0 /var/www/html/mediawiki
+chown -R apache:apache /var/www/html/mediawiki 
+service httpd restart
